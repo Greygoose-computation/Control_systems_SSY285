c2711a91cdb0244bd7841534245572497b5d9a52
Ananth Kaimal <specak30@gmail.com>
2024-11-28 15:54:58 +0100

    steps for discrete

---
+++ dscrt_test.m
@@ -0,0 +1,10 @@
+%--------------------------------------------------------------------------
+% This script is  a test for converting continues time structure into
+% discrete time structure 
+% Date - Nov 28 2024 
+% Author - AG Kaimal 
+%--------------------------------------------------------------------------
+% Declaring all the variables in the question  
+%--------------------------------------------------------------------------
+
+
